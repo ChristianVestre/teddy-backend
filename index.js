e785053cecfc7a55f8dfcf687c441884e39b3f82
Christian Vestre <christianvestre@ChristisMacBook.fritz.box>
2018-10-17 23:31:21 +0200

    trying to fix playground

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const {ApolloServer, gql } = require('apollo-server');
+const {ApolloServer, gql } = require('apollo-server-express');
 const typeDefs = require('./typeDefs.js')
 const resolvers = require('./resolvers.js')
 const database = require('./db.js')
+const express = require('express')
 const graphqlPath = process.env.GRAPHQL || 'graphql'
 const app = express()
+const PORT = process.env.PORT || 8000
 
-app.use(cors())
+
+//app.use(cors())
 
 server = new ApolloServer ({
     typeDefs,
@@ -17,6 +20,7 @@ server = new ApolloServer ({
 	introspection:true
 })
 server.applyMiddleware({ app, path: `/${graphqlPath}` })
-server.listen({ port: process.env.PORT || 5000 }).then(({url}) => {
+/*app.listen({ port: process.env.PORT || 5000 }).then(({url}) => {
     console.log(`🚀 Server ready at ${url}`)
-});
+});*/
+app.listen(PORT, () => console.log(`graphql listening on port ${PORT}`))
