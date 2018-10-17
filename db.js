f53b68aea51609f97fe7ae6d9711784e63b54b46
Christian Vestre <christianvestre@ChristisMacBook.fritz.box>
2018-10-17 22:07:17 +0200

    fixed postgres connection

--- db.js
+++ db.js
@@ -3,8 +3,6 @@ const Sequelize = require('sequelize');
 var db = new Sequelize(process.env.DATABASE_URL,{
     dialect: 'postgres',
     protocol: 'postgres',
-    port:match[4],
-    host:match[3],
     logging: true
   });
   
